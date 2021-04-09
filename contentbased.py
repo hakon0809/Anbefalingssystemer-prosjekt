8206d4b0f9c07afe8ea0baa10a4440f954c9098d
Hakon <hakon0809@hotmail.com>
2021-04-09 14:08:49 +0200

    added some poor performing recommendations

--- contentbased.py
+++ contentbased.py
@@ -8,15 +8,13 @@ from nltk.corpus import stopwords
 import re
 import time
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
-from rake_nltk import Rake
+# from rake_nltk import Rake
 from gensim.models import Word2Vec
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
+from evalMethods import evaluate_mse, evaluate_arhr, evaluate_recall 
+
+nltk.download('stopwords')
 
 pd.options.display.max_columns = 10
 pd.options.display.max_rows = 50
@@ -62,19 +60,27 @@ def remove_stopwords(words):
     return words
 
 
-def build_content_based_word2vec_similarity_matrix(df):
-
+def remap_df(df):
     # Remove front page events adressa.no
-    # also consist of all titles that have null value
+    # which also consist of all titles that have null value
     df = df.dropna(subset=['documentId'])
 
+    # remap document and userIds to integers starting from 0
+    df['documentId'] = pd.factorize(df['documentId'])[0]
+    df['userId'] = pd.factorize(df['userId'])[0]
+
+    #fill null active time with 3.6
+    df['activeTime'] = df['activeTime'].fillna(3.6).astype('float')
+    return df
+
+def build_content_based_word2vec_similarity_matrix(df):
+
+    df = df[['category', 'documentId', 'title']]
+
     # Drop duplicate 
-    df.drop_duplicates(subset=['userId', 'documentId'], inplace=True)
+    # df.drop_duplicates(subset=['userId', 'documentId'], inplace=True)
     df.drop_duplicates(subset=['documentId'], inplace=True)
 
-    # remap document and userids to integers starting from 0
-    df['userId'] = pd.factorize(df['userId'])[0]
-    df['documentId'] = pd.factorize(df['documentId'])[0]
     
     # combine category and title, doing some preprocessing
     df['category'] = df['category'].fillna("").astype('str')
@@ -95,11 +101,12 @@ def build_content_based_word2vec_similarity_matrix(df):
     
     # dictionary with vector values for each word
     vectors_dict = word_vec_model.wv
-    
+    print(len(df['titlecat'].index))
     # array of phrases from each titlecat row
     phrase_array = df['titlecat'].to_numpy()
     
-    # vector array for each phrase in each titlecat row
+    
+    # convert each word in titlecat into their vector representation
     vectors_array = df['titlecat'].apply(lambda words: [vectors_dict[x] for x in words]).to_numpy()
 
     # max_len = max(len(row) for row in vectors_array) #max number of words in titlecats is 31
@@ -119,17 +126,168 @@ def build_content_based_word2vec_similarity_matrix(df):
 
     #build similarity matrix for each titlecat row where the index [0][0] is the similarity of documentId with itself
     similarity_matrix = cosine_similarity(concatinated_matrix)
-    print(similarity_matrix[0][0])
+    # print(similarity_matrix[0][0])
     print('similarity matrix shape: '+ str(similarity_matrix.shape))
-    return df, similarity_matrix
+    return similarity_matrix
+
+def build_rating_matrix(df):
+    #ratingmatrix(users,items)
+    df = df[['userId','documentId', 'activeTime']]
+    
+    nr_users = len(df['userId'].drop_duplicates())
+    nr_documents = len(df['documentId'].drop_duplicates())
+
+    rating_matrix = np.zeros((nr_users,nr_documents))
 
 
+    #can be done faster using vectorization maybe
+    for index, row in df.iterrows():
+        rating_matrix[int(row[0])][int(row[1])] += row[2]
+
+
+    return rating_matrix
+
+def train_test_split(ratings, fraction=0.2):
+    """Leave out a fraction of dataset for test use"""
+    test = np.zeros(ratings.shape) # makes a empty matching list to ratings_table
+    train = ratings.copy() # makes a copy of the rating_table
+    for user in range(ratings.shape[0]): # Loop on the users in the ratings_table
+        # size(Int) = is the amount of ratings * fraction for that specific user
+        size = int(len(ratings[user, :].nonzero()[0]) * fraction) 
+        test_ratings = np.random.choice(ratings[user, :].nonzero()[0], 
+                                        size=size, 
+                                        replace=False)
+        train[user, test_ratings] = 0.
+        test[user, test_ratings] = ratings[user, test_ratings]
+    return train, test
+
+def predict_active_time(df,similarity_matrix, rating_matrix):
+    #sum(sigma (similarity * activetime))/sigma(similarity)
+    # similar to item-based neighborhood models - Adjusted cosine similarity
+    train, test = train_test_split(rating_matrix)
+    pred = []
+    actual = []
+    
+    #predict scores for all users based on the 20% test split for each user
+    for user in range(rating_matrix.shape[0]):
+        train_index_array = train[user, :].nonzero()[0]
+        train_value_array = rating_matrix[user,train_index_array]
+
+        test_index_array = test[user, :].nonzero()[0]
+        test_value_array = rating_matrix[user,test_index_array]
+        actual.append(test_value_array)
+        for index in test_index_array:
+            # 1d cosine similarity array between all train active_time scores and target test index
+            sim_array = similarity_matrix[train_index_array,index]
+            #summed product between cosine similarities and train active_time score
+            summed_rating = np.dot(sim_array, train_value_array)
+            #sum of train cosine similarity to target test index
+            sim_sum = np.sum(np.absolute(sim_array))
+            #predicted score
+            pred.append(summed_rating/sim_sum)
+
+    actual = [x for row in actual for x in row] 
+    print(pred[:10])
+    print(actual[:10])
+
+
+    return pred, actual
+
+#recommend most similar to item read with highest active time
+def content_recommendation_m1(rating_matrix, similarity_matrix, train, test):
+    pred = []
+    actual = []
+    #rating_matrix.shape[0]
+    result = []
+    for user in range(rating_matrix.shape[0]):
+        train_index_array = train[user, :].nonzero()[0]
+        train_value_array = rating_matrix[user,train_index_array]
+
+        test_index_array = test[user, :].nonzero()[0]
+        test_value_array = rating_matrix[user,test_index_array]
+        actual.append(test_index_array)
+
+        highest_train_active_time_doc = train_index_array[np.argmax(train_value_array)]
+        
+        nr_predictions = len(test_index_array) + 1
+        sim_array = similarity_matrix[highest_train_active_time_doc,:]
+        user_pred = []
+        while nr_predictions > 0:
+            prediction = np.argmax(sim_array)
+            sim_array[prediction] = 0
+
+            user_pred.append(prediction)
+            nr_predictions -= 1
+
+        pred.append(user_pred[1:])
+        tp = 0
+        for x in user_pred[1:]:
+            if x in test_index_array:
+                tp += 1
+        result.append((tp, evaluate_recall([user_pred[1:]], test_index_array)))
+    print(result)
+
+#recommend most similar to item with highest active time, not previously read
+def content_recommendation_m2(rating_matrix, similarity_matrix, train, test):
+    pred = []
+    actual = []
+    #rating_matrix.shape[0]
+    result = []
+    for user in range(rating_matrix.shape[0]):
+        train_index_array = train[user, :].nonzero()[0]
+        train_value_array = rating_matrix[user,train_index_array]
+
+        test_index_array = test[user, :].nonzero()[0]
+        test_value_array = rating_matrix[user,test_index_array]
+        actual.append(test_index_array)
+
+        highest_train_active_time_doc = train_index_array[np.argmax(train_value_array)]
+
+        nr_predictions = len(test_index_array) + 1
+        sim_array = similarity_matrix[highest_train_active_time_doc,:]
+        user_pred = []
+        while nr_predictions > 0:
+            prediction = np.argmax(sim_array)
+            sim_array[prediction] = 0
+            if not prediction in train_index_array:
+                user_pred.append(prediction)
+                nr_predictions -= 1
+            else:
+                pass
+
+        pred.append(user_pred[1:])
+        tp = 0
+        for x in user_pred[1:]:
+            if x in test_index_array:
+                tp += 1
+        result.append((tp, evaluate_recall([user_pred[1:]], test_index_array)))
+    print(result)
+
+def content_recommendation(rating_matrix, similarity_matrix):
+    #TODOES
+    #recommend most similar to last item read
+    #recommend most similar to k last items read
+    #bayes classifier maybe
+
+    train, test = train_test_split(rating_matrix)
+    content_recommendation_m1(rating_matrix, similarity_matrix, train, test)
+    content_recommendation_m2(rating_matrix, similarity_matrix, train, test)
+    return
 
     
 
 if __name__ == '__main__':
-    # df=load_data('active1000')
-    df=load_data_test()
-    
-    build_content_based_word2vec_similarity_matrix(df)
+    # df = load_data('active1000')
+    df = load_data_test()
 
+    df = remap_df(df)
+    similarity_matrix = build_content_based_word2vec_similarity_matrix(df)
+
+    rating_matrix = build_rating_matrix(df)
+    pred, actual = predict_active_time(df, similarity_matrix, rating_matrix)
+    print(evaluate_mse(pred, actual))
+
+
+    content_recommendation(rating_matrix,similarity_matrix)
+
+ 
